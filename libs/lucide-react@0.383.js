13a1a2c1265ced88172575cfb6e9e64b4c2af84b
trungaiavietnam-ui <trungaiavietnam@gmail.com>
2026-06-08 21:40:57 +0700

    Add files via upload

---
+++ libs/lucide-react@0.383.js
@@ -0,0 +1,3 @@
+/* esm.sh - lucide-react@0.383.0 */
+import "/react@^16.5.1%20||%20^17.0.0%20||%20^18.0.0?target=es2022";
+export * from "/lucide-react@0.383.0/es2022/lucide-react.bundle.mjs";
---
+++ libs/react@18.2.js
@@ -0,0 +1,3 @@
+/* esm.sh - react@18.2.0 */
+export * from "/react@18.2.0/es2022/react.bundle.mjs";
+export { default } from "/react@18.2.0/es2022/react.bundle.mjs";
