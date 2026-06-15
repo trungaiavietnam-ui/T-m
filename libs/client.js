332b747f7a62cf37f22e6054cbdda242bd7a731c
trungaiavietnam-ui <trungaiavietnam@gmail.com>
2026-06-15 21:06:21 +0700

    Delete libs directory

--- libs/client.js
+++
@@ -1,3 +0,0 @@
-/* esm.sh - react-dom@18.2.0/client */
-export * from "/react-dom@18.2.0/X-ZXJlYWN0/es2022/client.bundle.mjs";
-export { default } from "/react-dom@18.2.0/X-ZXJlYWN0/es2022/client.bundle.mjs";
