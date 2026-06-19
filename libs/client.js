b80bc131982e830c5d82a1ce52564abdccbb10df
trungaiavietnam-ui <trungaiavietnam@gmail.com>
2026-06-19 16:48:38 +0700

    Add files via upload

---
+++ libs/client.js
@@ -0,0 +1,3 @@
+/* esm.sh - react-dom@18.2.0/client */
+export * from "/react-dom@18.2.0/X-ZXJlYWN0/es2022/client.bundle.mjs";
+export { default } from "/react-dom@18.2.0/X-ZXJlYWN0/es2022/client.bundle.mjs";
---
+++ libs/react-toastify@9.1.js
@@ -0,0 +1,3 @@
+/* esm.sh - react-toastify@9.1.3 */
+import "/react@>=16?target=es2022";
+export * from "/react-toastify@9.1.3/es2022/react-toastify.bundle.mjs";
